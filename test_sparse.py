9911a32e5ba5fa78ac55731439b53110391a9ba9
Daniel Klein <othercriteria@gmail.com>
2012-07-11 17:42:10 -0400

    Added multiple covariates, consideration of nonstationary model without covariates, and other various improvements.

--- test_sparse.py
+++ test_sparse.py
@@ -10,9 +10,9 @@ from Models import Stationary, StationaryLogistic, NonstationaryLogistic
 
 # Parameters
 params = { 'file_network': 'data/cit-HepTh/cit-HepTh.txt',
-           'import_limit_edges': 500,
+           'import_limit_edges': 1200,
            'file_dates': 'data/cit-HepTh/cit-HepTh-dates.txt',
-           'pub_diff_cutoff': 100 }
+           'pub_diff_classes': [30, 60, 90, 180, 360, 720] }
 
 
 # Import covariate data from file
@@ -35,6 +35,7 @@ for line in open(params['file_network'], 'r').readlines():
     if line == '': continue
     n_1, n_2 = line.split('\t')
     if not (n_1 in dates and n_2 in dates): continue
+    if dates[n_1] < dates[n_2]: continue
     edges.append((n_1, n_2))
     if len(edges) >= params['import_limit_edges']: break
 
@@ -43,15 +44,20 @@ net = Network()
 net.network_from_edges(edges)
 net.show_degree_histograms()
 
-# Convert covariate data to covariate
-cov = net.new_edge_covariate('short_pub_diff')
-def f_similar_pub_date(n_1, n_2):
-    return abs(dates[n_1] - dates[n_2]) < timedelta(params['pub_diff_cutoff'])
-cov.from_binary_function_name(f_similar_pub_date)
+# Convert covariate data to covariates
+cov_names = []
+for l, u in zip([0] + params['pub_diff_classes'], params['pub_diff_classes']):
+    cov_name = 'pub_diff_%d-%d' % (l, u)
+    cov_names.append(cov_name)
+    cov = net.new_edge_covariate(cov_name)
+    def f_pub_date_diff_in_range(n_1, n_2):
+        return timedelta(l) <= dates[n_1] - dates[n_2] < timedelta(u)
+    cov.from_binary_function_name(f_pub_date_diff_in_range)
 
 # Add publication order node covariate
 pub_dates = [dates[p].toordinal() for p in net.names]
 net.new_node_covariate('pub_dates').from_pairs(net.names, pub_dates)
+net.node_covariates['pub_dates'].show_histogram()
 net.show_heatmap('pub_dates')
 
 # Exclude impossible edges using infinite offset
@@ -63,18 +69,23 @@ def f_impossible_pub_order(n_1, n_2):
 net.initialize_offset().from_binary_function_name(f_impossible_pub_order)
 
 # Fit model
-for model_name, model in [('Stationary', Stationary()),
-                          ('Stationary Logistic', StationaryLogistic()),
-                          ('Nonstationary Logistic', NonstationaryLogistic())]:
-    print model_name
-    if not model_name == 'Stationary':
-        model.beta['short_pub_diff'] = None
+for name, model, use_covs in [('Stationary', Stationary(), False),
+                              ('Stationary', StationaryLogistic(), True),
+                              ('Nonstationary', NonstationaryLogistic(), False),
+                              ('Nonstationary', NonstationaryLogistic(), True)]:
+    print name
+    if use_covs:
+        for cov_name in cov_names:
+            model.beta[cov_name] = None
     model.fit_convex_opt(net)
-    if not model_name == 'Stationary':
-        print 'short_pub_diff: %.2f' % model.beta['short_pub_diff']
+    print 'NLL: %.2f' % model.nll(net)
     print 'kappa: %.2f' % model.kappa
+    if use_covs:
+        for cov_name in cov_names:
+            print '%s: %.2f' % (cov_name, model.beta[cov_name])
     print '\n'
 
-# Redisplay heatmap, ordered by estimated alphas from NonstationaryLogistic fit
+# Redisplay heatmap, ordered by estimated alphas from last fit, i.e.,
+# NonstationaryLogistic with publication date difference covariates
 net.show_heatmap('alpha_out')
 net.show_heatmap('alpha_in')
