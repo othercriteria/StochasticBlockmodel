f467a61898b9507825cae0c3a157bb0e77cd3a97
Daniel Klein <othercriteria@gmail.com>
2012-08-22 20:28:13 -0400

    Major changes to results that are calculated and output.

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import numpy as np
 from Network import Network
 from Models import StationaryLogistic, NonstationaryLogistic
 from Models import alpha_zero, alpha_norm, alpha_unif, alpha_gamma
-from Experiment import RandomSubnetworks, Results, add_network_stats
+from Experiment import RandomSubnetworks, Results, add_network_stats, rel_mse
 from Utility import logit
 
 # Parameters
@@ -17,16 +17,18 @@ params = { 'N': 300,
            'beta_sd': 1.0,
            'x_diff_cutoff': 0.3,
            'alpha_unif_sd': 0.0,
-           'alpha_norm_sd': 1.0,
+           'alpha_norm_sd': 0.0,
            'alpha_gamma_sd': 0.0,
-           'kappa_target': ('degree', 2),
-           'offset_extreme': False,
-           'fit_nonstationary': False,
+           'kappa_target': ('density', 0.1),
+           'offset_extremes': False,
+           'fit_nonstationary': True,
            'fit_method': 'convex_opt',
-           'num_reps': 15,
-           'sub_sizes': range(10, 110, 10),
+           'num_reps': 5,
+           'sub_sizes': range(10, 60, 10),
+           'verbose': False,
            'plot_mse': True,
-           'plot_network': True }
+           'plot_network': False,
+           'plot_fit_info': False }
 
 
 # Set random seed for reproducible output
@@ -83,17 +85,37 @@ for c in covariates:
     f_true, f_estimated = f_c(c)
     results.new('True beta_{%s}' % c, 'm', f_true)
     results.new('Estimated beta_{%s}' % c, 'm', f_estimated)
-results.new('MSE(P_ij)', 'nm',
-            lambda n, d, f: np.mean((d.edge_probabilities(n) - \
-                                     f.edge_probabilities(n))**2))
-if params['offset_extreme']:
-    results.new('Active cells', 'n',
-                lambda n: np.isfinite(n.offset.matrix()).sum())
+if params['offset_extremes']:
+    results.new('# Active', 'n', lambda n: np.isfinite(n.offset.matrix()).sum())
 else:
-    results.new('MSE(logit P_ij)', 'nm',
-            lambda n, d, f: np.mean((logit(d.edge_probabilities(n)) - \
-                                     logit(f.edge_probabilities(n)))**2))
-    results.new('Active cells', 'n', lambda n: n.N ** 2)
+    results.new('# Active', 'n', lambda n: n.N ** 2)
+if params['fit_nonstationary']:
+    import scipy.optimize as opt
+    def rel_mse_p_ij(n, d, f):
+        P = d.edge_probabilities(n)
+        return rel_mse(f.edge_probabilities(n), f.baseline(n), P)
+    results.new('Rel. MSE(P_ij)', 'nm', rel_mse_p_ij)
+    if not params['offset_extremes']:
+        def rel_mse_logit_p_ij(n, d, f):
+            logit_P = logit(d.edge_probabilities(n))
+            logit_Q = f.baseline_logit(n)
+            return rel_mse(logit(f.edge_probabilities(n)), logit_Q, logit_P)
+        results.new('Rel. MSE(logit P_ij)', 'nm', rel_mse_logit_p_ij)
+else:
+    def rel_mse_p_ij(n, d, f):
+        return rel_mse(f.edge_probabilities(n), d.baseline(n),
+                       d.edge_probabilities(n))
+    results.new('Rel. MSE(P_ij)', 'nm', rel_mse_p_ij)
+    def rel_mse_logit_p_ij(n, d, f):
+        return rel_mse(logit(f.edge_probabilities(n)), d.baseline_logit(n),
+                       logit(d.edge_probabilities(n)))
+    results.new('Rel. MSE(logit P_ij)', 'nm', rel_mse_logit_p_ij)
+
+if params['fit_method'] == 'convex_opt':
+    results.new('Obj. evals.', 'm', lambda d, f: f.fit_info['obj_evals'])
+    results.new('Wall time (sec.)', 'm', lambda d, f: f.fit_info['wall_time'])
+    results.new('||ET_final - T||_2', 'm',
+                lambda d, f: np.sqrt(np.sum((f.fit_info['grad_final'])**2)))
 
 for sub_size in params['sub_sizes']:
     print 'subnetwork size = %d' % sub_size
@@ -103,10 +125,14 @@ for sub_size in params['sub_sizes']:
         subnet = gen.sample()
         data_model.match_kappa(subnet, params['kappa_target'])
         subnet.generate(data_model)
-        if params['offset_extreme']: subnet.offset_extremes()
+        if params['offset_extremes']: subnet.offset_extremes()
 
         if params['fit_method'] == 'convex_opt':
-            fit_model.fit_convex_opt(subnet)
+            if params['verbose']:
+                fit_model.fit_convex_opt(subnet, verbose = True)
+                print
+            else:
+                fit_model.fit_convex_opt(subnet)
         elif params['fit_method'] == 'logistic':
             fit_model.fit_logistic(subnet)
         elif params['fit_method'] == 'logistic_l2':
@@ -121,23 +147,29 @@ for c in covariates:
     covariate_mses.append(name)
     results.estimate_mse(name, 'True beta_{%s}' % c, 'Estimated beta_{%s}' % c)
 
-# Plot inference performace, in terms of MSE(beta) and MSE(P_ij); also
-# plot kappas chosen for data models
+# Plot inference performace, in terms of MSE(beta) and MSE(P_ij)
 if params['plot_mse']:
     to_plot = [(['MSE(beta_i)'] + covariate_mses,
                 {'ymin': 0, 'ymax': 3.0, 'plot_mean': True}),
-               ('MSE(P_ij)', {'ymin': 0, 'ymax': 1}),
-               ('Active cells', {'ymin': 0}),
-               'Sample kappa']
-    if not params['offset_extreme']:
-        to_plot.insert(2, ('MSE(logit P_ij)', {'ymin': 0, 'ymax': 6}))
+               ('Rel. MSE(P_ij)', {'ymin': 0, 'ymax': 2, 'baseline': 1}),
+               ('# Active', {'ymin': 0})]
+    if not (params['fit_nonstationary'] and params['offset_extremes']):
+        to_plot.insert(2, ('Rel. MSE(logit P_ij)',
+                           {'ymin':0, 'ymax': 2, 'baseline': 1}))
     results.plot(to_plot)
   
-# Plot network statistics as well as sparsity parameter
+# Plot network statistics
 if params['plot_network']:
     results.plot([('Average degree', {'ymin': 0, 'plot_mean': True}),
                   (['Out-degree', 'Max out-degree', 'Min out-degree'],
                    {'ymin': 0, 'plot_mean': True}),
                   (['In-degree', 'Max out-degree', 'Min in-degree'],
                    {'ymin': 0, 'plot_mean': True}),
-                  ('Self-loop density', {'ymin': 0, 'plot_mean': True})])
+                  ('Self-loop density', {'ymin': 0, 'plot_mean': True}),
+                  'Sample kappa'])
+
+# Plot convex optimization fitting internal details
+if params['plot_fit_info']:
+    results.plot([('Obj. evals.', {'ymin': 0}),
+                  ('Wall time (sec.)', {'ymin': 0}),
+                  ('||ET_final - T||_2', {'ymin': 0})])
