9563a11857fff18ef5ec520bce0191b76744e6a6
Daniel Klein <othercriteria@gmail.com>
2013-04-05 07:55:05 -0400

    Lots of changes to match changes in data generation and inference.

--- test_regular.py
+++ test_regular.py
@@ -10,22 +10,21 @@
 import numpy as np
 
 from Network import Network
-from Models import StationaryLogistic, RaschLogistic, StationaryLogisticMargins
-from Experiment import RandomSubnetworks, Results, add_network_stats
+from Models import StationaryLogistic, NonstationaryLogistic, FixedMargins
+from Experiment import RandomSubnetworks, Results
 
 # Parameters
-params = { 'N': 150,
+params = { 'N': 300,
            'B': 5,
            'beta_sd': 1.0,
            'x_diff_cutoff': 0.3,
-           'margin_scaling': ('degree', 2),
-           'gibbs_cover': 1,
-           'fit_nonstationary': False,
-           'num_reps': 5,
-           'sub_sizes': range(10, 80, 10),
-           'plot_mse_beta': True,
-           'plot_network': True }
-
+           'margin_scaling': ('density', 0.1),
+           'arbitrary_init': False,
+           'gibbs_covers': [1.0],
+           'fit_nonstationary': True,
+           'num_reps': 10,
+           'sub_sizes': range(10, 110, 10),
+           'plot_mse_beta': True }
 
 # Set random seed for reproducible output
 np.random.seed(137)
@@ -39,18 +38,21 @@ for field in params:
 net = Network(params['N'])
 
 # Generate covariates and associated coefficients
-data_model = StationaryLogisticMargins()
+data_base_model = StationaryLogistic()
 covariates = []
 for b in range(params['B']):
     name = 'x_%d' % b
     covariates.append(name)
 
-    data_model.beta[name] = np.random.normal(0, params['beta_sd'])
+    data_base_model.beta[name] = np.random.normal(0, params['beta_sd'])
 
     x_node = np.random.normal(0, 1, params['N'])
     def f_x(i_1, i_2):
         return abs(x_node[i_1] - x_node[i_2]) < params['x_diff_cutoff']
     net.new_edge_covariate(name).from_binary_function_ind(f_x)
+data_model = FixedMargins(data_base_model)
+net.new_node_covariate_int('r')
+net.new_node_covariate_int('c')
 
 if params['fit_nonstationary']:
     fit_model = NonstationaryLogistic()
@@ -60,15 +62,18 @@ for c in covariates:
     fit_model.beta[c] = None
 
 # Set up recording of results from experiment
-results = Results(params['sub_sizes'], params['num_reps'])
-add_network_stats(results)
-def f_c(c):
-    return (lambda d, f: d.beta[c]), (lambda d, f: f.beta[c])
-for c in covariates:
-    # Need to do this hackily to avoid for-loop/lambda-binding weirdness.
-    f_true, f_estimated = f_c(c)
-    results.new('True beta_{%s}' % c, 'm', f_true)
-    results.new('Estimated beta_{%s}' % c, 'm', f_estimated)
+gibbs_results = {}
+for gibbs_cover in params['gibbs_covers']:
+    results = Results(params['sub_sizes'], params['num_reps'],
+                      'Gibbs cover: %.2f' % gibbs_cover)
+    def f_c(c):
+        return (lambda d, f: d.base_model.beta[c]), (lambda d, f: f.beta[c])
+    for c in covariates:
+        # Need to do this hackily to avoid for-loop/lambda-binding weirdness.
+        f_true, f_estimated = f_c(c)
+        results.new('True beta_{%s}' % c, 'm', f_true)
+        results.new('Est. beta_{%s}' % c, 'm', f_estimated)
+    gibbs_results[gibbs_cover] = results
 
 for sub_size in params['sub_sizes']:
     print 'Subnetwork size = %d' % sub_size
@@ -84,30 +89,24 @@ for sub_size in params['sub_sizes']:
         elif scaling == 'density':
             m = int(value * sub_size)
             r, c = np.repeat(m, sub_size), np.repeat(m, sub_size)
-        subnet.generate(data_model, r, c, params['gibbs_cover'])
+        subnet.node_covariates['r'][:] = r
+        subnet.node_covariates['c'][:] = c
+        
+        for gibbs_cover in params['gibbs_covers']:
+            subnet.generate(data_model, coverage = gibbs_cover,
+                            arbitrary_init = params['arbitrary_init'])
+            fit_model.fit_convex_opt(subnet)
+            gibbs_results[gibbs_cover].record(sub_size, rep,
+                                              subnet, data_model, fit_model)
 
-        fit_model.fit_convex_opt(subnet)
-
-        results.record(sub_size, rep, subnet, data_model, fit_model)
-
-# Compute beta MSEs
-covariate_mses = []
-for c in covariates:
-    name = 'MSE(beta_{%s})' % c
-    covariate_mses.append(name)
-    results.estimate_mse(name, 'True beta_{%s}' % c, 'Estimated beta_{%s}' % c)
-
-# Plot inference performace, in terms of MSE(beta)
-if params['plot_mse_beta']:
+# Compute beta MSEs and plot performance in terms of MSE(beta)
+for sub, gibbs_cover in enumerate(sorted(gibbs_results)):
+    results = gibbs_results[gibbs_cover]
+    covariate_mses = []
+    for c in covariates:
+        name = 'MSE(beta_{%s})' % c
+        covariate_mses.append(name)
+        results.estimate_mse(name, 'True beta_{%s}' % c, 'Est. beta_{%s}' % c)
     results.plot([(['MSE(beta_i)'] + covariate_mses,
                    {'ymin': 0, 'ymax': 3.0, 'plot_mean': True})])
-  
-# Plot network statistics as well as sparsity parameter
-if params['plot_network']:
-    results.plot([('Average degree', {'ymin': 0, 'plot_mean': True}),
-                  (['Out-degree', 'Max out-degree', 'Min out-degree'],
-                   {'ymin': 0, 'plot_mean': True}),
-                  (['In-degree', 'Max out-degree', 'Min in-degree'],
-                   {'ymin': 0, 'plot_mean': True}),
-                  ('Self-loop density', {'ymin': 0, 'plot_mean': True})])
 
