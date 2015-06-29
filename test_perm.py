34f4c70af1bd5a98e9e6334825a335ab3918a773
Daniel Klein <othercriteria@gmail.com>
2015-06-29 11:04:23 -0400

    Adding in more untracked scripts

---
+++ test_perm.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+
+# Test of "new style" network inference
+# Daniel Klein, 5/16/2012
+
+import numpy as np
+
+from Network import Network
+from Models import StationaryLogistic, NonstationaryLogistic
+from Models import FixedMargins
+from Models import alpha_zero
+from Experiment import RandomSubnetworks, Results, add_network_stats, rel_mse
+from Utility import logit
+
+# Parameters
+params = { 'N': 600,
+           'B': 1,
+           'theta_sd': 1.0,
+           'theta_fixed': { 'x_0': 2.0, 'x_1': -1.0 },           
+           'fisher_information': False,
+           'baseline': False,
+           'fit_nonstationary': False,
+           'fit_method': 'conditional',
+           'p_approx': 'canfield',
+           'num_reps': 15,
+           'sub_sizes': np.floor(np.logspace(1, 2.1, 20)),
+           'verbose': False,
+           'plot_mse': True,
+           'plot_network': False,
+           'plot_fit_info': False }
+
+
+# Set random seed for reproducible output
+np.random.seed(137)
+
+# Initialize full network
+net = Network(params['N'])
+alpha_zero(net)
+
+# Generate covariates and associated coefficients
+data_model = NonstationaryLogistic()
+covariates = []
+for b in range(params['B']):
+    name = 'x_%d' % b
+    covariates.append(name)
+
+    if name in params['theta_fixed']:
+        data_model.beta[name] = params['theta_fixed'][name]
+    else:
+        data_model.beta[name] = np.random.normal(0, params['theta_sd'])
+
+    def f_x(i_1, i_2):
+        return np.random.uniform(-np.sqrt(3), np.sqrt(3))
+    net.new_edge_covariate(name).from_binary_function_ind(f_x)
+
+# Specify data model as generation permuation networks
+net.new_node_covariate_int('r')[:] = 1
+net.new_node_covariate_int('c')[:] = 1
+data_model = FixedMargins(data_model, 'r', 'c', coverage = 0.5)
+
+if params['fit_nonstationary']:
+    fit_model = NonstationaryLogistic()
+else:
+    fit_model = StationaryLogistic()
+for c in covariates:
+    fit_model.beta[c] = None
+
+# Set up recording of results from experiment
+results = Results(params['sub_sizes'], params['num_reps'])
+add_network_stats(results)
+def true_est_theta_c(c):
+    return (lambda d, f: d.base_model.beta[c]), (lambda d, f: f.beta[c])
+for c in covariates:
+    # Need to do this hackily to avoid for-loop/lambda-binding weirdness.
+    f_true, f_est = true_est_theta_c(c)
+    results.new('True theta_{%s}' % c, 'm', f_true)
+    results.new('Est. theta_{%s}' % c, 'm', f_est)
+results.new('# Active', 'n', lambda n: n.N ** 2)
+if params['fisher_information']:
+    def info_theta_c(c):
+        def f_info_theta_c(d, f):
+            return d.I_inv['theta_{%s}' % c]
+        return f_info_theta_c
+    for c in covariates:
+        results.new('Info theta_{%s}' % c, 'm', info_theta_c(c))
+if params['baseline']:
+    def rel_mse_p_ij(n, d, f):
+        P = d.edge_probabilities(n)
+        return rel_mse(f.edge_probabilities(n), f.baseline(n), P)
+    results.new('Rel. MSE(P_ij)', 'nm', rel_mse_p_ij)
+    def rel_mse_logit_p_ij(n, d, f):
+        logit_P = logit(d.edge_probabilities(n))
+        logit_Q = f.baseline_logit(n)
+        return rel_mse(logit(f.edge_probabilities(n)), logit_Q, logit_P)
+    results.new('Rel. MSE(logit P_ij)', 'nm', rel_mse_logit_p_ij)
+
+if params['fit_method'] in ['convex_opt', 'conditional',
+                            'irls', 'conditional_is']:
+    results.new('Wall time (sec.)', 'm', lambda d, f: f.fit_info['wall_time'])
+if params['fit_method'] in ['convex_opt', 'conditional', 'conditional_is']:
+    def work(f):
+        w = 0
+        for work_type in ['nll_evals', 'grad_nll_evals', 'cnll_evals']:
+            if work_type in f.fit_info:
+                w += f.fit_info[work_type]
+        return w
+    results.new('Work', 'm', lambda d, f: work(f))
+    results.new('||ET_final - T||_2', 'm',
+                lambda d, f: np.sqrt(np.sum((f.fit_info['grad_nll_final'])**2)))
+
+for sub_size in params['sub_sizes']:
+    print 'subnetwork size = %d' % sub_size
+
+    gen = RandomSubnetworks(net, sub_size)
+
+    for rep in range(params['num_reps']):
+        subnet = gen.sample()
+        subnet.generate(data_model)
+
+        if params['fisher_information']:
+            data_model.fisher_information(subnet)
+        
+        if params['fit_method'] == 'convex_opt':
+            if params['verbose']:
+                fit_model.fit_convex_opt(subnet, verbose = True)
+                print
+            else:
+                fit_model.fit_convex_opt(subnet)
+        if params['fit_method'] == 'irls':
+            fit_model.fit_irls(subnet)
+        elif params['fit_method'] == 'logistic':
+            fit_model.fit_logistic(subnet)
+        elif params['fit_method'] == 'logistic_l2':
+            fit_model.fit_logistic_l2(subnet, prior_precision = 1.0)
+        elif params['fit_method'] == 'mh':
+            for c in covariates:
+                fit_model.beta[c] = 0.0
+            fit_model.fit_mh(subnet)
+        elif params['fit_method'] == 'conditional':
+            fit_model.fit_conditional(subnet, verbose = True,
+                                      p_approx = params['p_approx'])
+        elif params['fit_method'] == 'conditional_is':
+            fit_model.fit_conditional(subnet, T = 50, verbose = True,
+                                      p_approx = params['p_approx'])
+        elif params['fit_method'] == 'composite':
+            fit_model.fit_composite(subnet, T = 100, verbose = True)
+        elif params['fit_method'] == 'brazzale':
+            fit_model.fit_brazzale(subnet)
+        elif params['fit_method'] == 'saddlepoint':
+            fit_model.fit_saddlepoint(subnet)
+            fit_model.fit_convex_opt(subnet, fix_beta = True)
+        elif params['fit_method'] == 'none':
+            pass
+            
+        results.record(sub_size, rep, subnet, data_model, fit_model)
+
+# Compute beta MSEs
+covariate_mses = []
+for c in covariates:
+    name = 'MSE(theta_{%s})' % c
+    covariate_mses.append(name)
+    results.estimate_mse(name, 'True theta_{%s}' % c, 'Est. theta_{%s}' % c)
+results.summary()
+
+# Plot inference performace, in terms of MSE(theta) and MSE(P_ij)
+if params['plot_mse']:
+    to_plot = []
+    if not params['fit_method'] == 'none':
+        to_plot.append((['MSE(theta_i)'] + covariate_mses,
+                        {'ymin': 0, 'ymax': 0.5, 'plot_mean': True}))
+    if params['baseline']:
+        to_plot.append(('Rel. MSE(P_ij)',
+                        {'ymin': 0, 'ymax': 2, 'baseline': 1}))
+        to_plot.append(('Rel. MSE(logit P_ij)',
+                        {'ymin':0, 'ymax': 2, 'baseline': 1}))
+    to_plot.append(('# Active', {'ymin': 0}))
+    if params['fisher_information']:
+        to_plot.append((['Info theta_i'] + \
+                        ['Info theta_{%s}' % c for c in covariates],
+                        {'ymin': 0, 'plot_mean': True}))
+    results.plot(to_plot)
+
+    to_plot = []
+    to_plot.append((['MSE(theta_i)'] + covariate_mses,
+                    {'plot_mean': True, 'loglog': True}))
+    if params['fisher_information']:
+        to_plot.append((['Info theta_i'] + \
+                        ['Info theta_{%s}' % c for c in covariates],
+                        {'plot_mean': True, 'loglog': True}))
+    results.plot(to_plot)
+  
+# Plot network statistics
+if params['plot_network']:
+    to_plot = [('Average degree', {'ymin': 0, 'plot_mean': True}),
+               (['Out-degree', 'Max out-degree', 'Min out-degree'],
+                {'ymin': 0, 'plot_mean': True}),
+               (['In-degree', 'Max out-degree', 'Min in-degree'],
+                {'ymin': 0, 'plot_mean': True}),
+               ('Self-loop density', {'ymin': 0, 'plot_mean': True})]
+    results.plot(to_plot)
+
+# Plot convex optimization fitting internal details
+if (params['plot_fit_info'] and params['fit_method'] == 'irls'):
+    results.plot([('Wall time (sec.)', {'ymin': 0})])
+if (params['plot_fit_info'] and
+    params['fit_method'] in ['convex_opt', 'conditional', 'conditional_is']):
+    results.plot([('Work', {'ymin': 0}),
+                  ('Wall time (sec.)', {'ymin': 0}),
+                  ('||ET_final - T||_2', {'ymin': 0})])
+
+# Report parameters for the run
+print 'Parameters:'
+for field in params:
+    print '%s: %s' % (field, repr(params[field]))
---
+++ test_rasch.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from BinaryMatrix import approximate_conditional_nll as acnll
+from BinaryMatrix import approximate_from_margins_weights as sample
+
+# Parameters
+M = 50
+N = 4
+theta = 2.0
+kappa = -1.628
+alpha_min = -0.86
+beta_min = -0.4
+alpha_level = 0.2
+n_MC = 20
+trials = 50
+L = 61
+theta_l_min = -6.0
+theta_l_max = 6.0
+
+# Generate theta grid for inference
+theta_grid = np.linspace(theta_l_min, theta_l_max, L)
+
+# Generate covariate
+v = np.random.normal(0, 1.0, (M,N))
+
+# Generate Bernoulli probabilities according to logistic regression model
+logit_P = np.zeros((M,N))
+for i in range(1,M):
+    logit_P[i,:] += np.random.uniform(alpha_min, alpha_min+1)
+for j in range(1,N):
+    logit_P[:,j] += np.random.uniform(beta_min, beta_min+1)
+logit_P += kappa
+logit_P += theta * v
+P = 1.0 / (1.0 + np.exp(-logit_P))
+
+# Do experiment
+in_interval = np.empty(trials)
+length = np.empty(trials)
+for trial in range(trials):
+    # Generate data for this trial
+    X = np.random.random((M,N)) < P
+
+    # Observed statistic
+    t_X = np.sum(X * v)
+
+    # Row and column margins; the part of the data we can use to design Q
+    r, c = X.sum(1), X.sum(0)
+
+    # Generate samples from the mixture proposal distribution
+    Y = []
+    for n in range(n_MC):
+        l = np.random.randint(L)
+        logit_P_l = theta_grid[l] * v
+        
+        Y_sparse = sample(r, c, np.exp(logit_P_l))
+        Y_dense = np.zeros((M,N), dtype = np.bool)
+        for i, j in Y_sparse:
+            if i == -1: break
+            Y_dense[i,j] = 1
+        Y.append(Y_dense)
+
+    # Statistics for the samples from the proposal distribution only
+    # need to be calculated once...
+    t_Y = np.empty(n_MC)
+    for n in range(n_MC):
+        t_Y[n] = np.sum(Y[n] * v)
+    I_t_Y_plus = t_Y >= t_X
+    I_t_Y_minus = -t_Y >= -t_X
+
+    # Probabilities under each component of the proposal distribution
+    # only need to be calculated once...
+    log_Q_X = np.empty(L)
+    log_Q_Y = np.empty((L,n_MC))
+    for l in range(L):
+        logit_P_l = theta_grid[l] * v
+        log_Q_X[l] = -acnll(X, np.exp(logit_P_l))
+        for n in range(n_MC):
+            log_Q_Y[l,n] = -acnll(Y[n], np.exp(logit_P_l))
+    Q_sum_X = np.exp(np.logaddexp.reduce(log_Q_X))
+    Q_sum_Y = np.empty(n_MC)
+    for n in range(n_MC):
+        Q_sum_Y[n] = np.exp(np.logaddexp.reduce(log_Q_Y[:,n]))
+
+    # Step over the grid, calculating approximate p-values
+    p_plus = np.empty(L)
+    p_minus = np.empty(L)
+    for l in range(L):
+        theta_l = theta_grid[l]
+
+        p_num_plus, p_num_minus, p_denom = 0.0, 0.0, 0.0
+
+        # X contribution
+        w_X = np.exp(theta_l * t_X) / Q_sum_X
+        p_num_plus += w_X
+        p_num_minus += w_X
+        p_denom += w_X
+
+        # Y contribution
+        for n in range(n_MC):
+            w_Y = np.exp(theta_l * t_Y[n]) / Q_sum_Y[n]
+            if I_t_Y_plus[n]: p_num_plus += w_Y
+            if I_t_Y_minus[n]: p_num_minus += w_Y
+            p_denom += w_Y
+
+        p_plus[l] = p_num_plus / p_denom
+        p_minus[l] = p_num_minus / p_denom
+
+    p_plus_minus = np.fmin(1, 2 * np.fmin(p_plus, p_minus))
+
+    C_alpha = theta_grid[p_plus_minus > alpha_level]
+    C_alpha_l, C_alpha_u = np.min(C_alpha), np.max(C_alpha)
+    if C_alpha_l == theta_l_min:
+        C_alpha_l = -np.inf
+    if C_alpha_u == theta_l_max:
+        C_alpha_u = np.inf
+
+    print '[%.2f, %.2f]' % (C_alpha_l, C_alpha_u)
+
+    in_interval[trial] = C_alpha_l <= theta <= C_alpha_u
+    length[trial] = C_alpha_u - C_alpha_l
+
+# Summarize results
+print 'Coverage probability: %.2f' % np.mean(in_interval)
+print 'Median length: %.2f' % np.median(length)
