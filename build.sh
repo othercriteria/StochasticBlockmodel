fb4b6b2e9b67bc199df039907315fc75e30998d6
Daniel Klein <othercriteria@gmail.com>
2015-07-22 17:38:52 -0400

    Fix building support code on Ubuntu...

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 # Compiles C code until I get around to setting up a proper build system.
-# For now, this is OS X-centric.
 # Daniel Klein, 12/4/2012
 
 gcc -O3 -fPIC -c support_BinaryMatrix.c -o support_BinaryMatrix.o
-ld -dylib -macosx_version_min 10.7 -o support.so -lc -lm -lgmp support_BinaryMatrix.o outside_code/exact_v0.21/code/C_version/exact_library.o
+gcc -shared -o support.so -lc -lm -lgmp support_BinaryMatrix.o
 
