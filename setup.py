3dadfc77edaf6ae4c2398b7a4fce8a73b69a3ff9
Daniel Klein <othercriteria@gmail.com>
2016-01-01 23:24:46 -0500

    Stubbing out setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'ArrayStats',
+      version = '0.1.0',
+      description = 'Inference for data structured as two-dimensional arrays.',
+      author = 'Daniel Klein',
+      author_email = 'othercriteria@gmail.com',
+      url = 'https://github.com/othercriteria/StochasticBlockmodel',
+      )
