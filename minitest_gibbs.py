7e631db22e22efcec456a84b0584e29cb3efae7a
Daniel Klein <othercriteria@gmail.com>
2016-03-23 10:56:58 -0400

    Various fixes and improvements

--- minitest_gibbs.py
+++ minitest_gibbs.py
@@ -12,8 +12,14 @@ from Models import StationaryLogistic, NonstationaryLogistic, Blockmodel
 from Models import alpha_zero, alpha_norm
 from Experiment import minimum_disagreement
 
-N = 100
-reps = 150
+# Parameters
+N = 30
+alpha_sd = 1.0
+from_truth = True
+steps = 100
+
+# Set random seed for reproducible outputs
+np.random.seed(137)
 
 net = Network(N)
 
@@ -37,21 +43,23 @@ for name, theta in [('ll', 4.0),
                     ('rr', 3.0),
                     ('lr', -2.0)]:
     data_model.beta[name] = theta
-alpha_norm(net, 3.0)
+alpha_norm(net, alpha_sd)
 data_model.match_kappa(net, ('row_sum', 2))
 net.generate(data_model)
 net.show_heatmap()
 net.offset_extremes()
 
-fit_base_model = StationaryLogistic()
+fit_base_model = NonstationaryLogistic()
 fit_base_model.beta['x'] = None
 fit_model = Blockmodel(fit_base_model, 2)
 #fit_model.base_model.fit = fit_model.base_model.fit_conditional
 
-# Initialize from truth...
+# Initialize block assignments
 net.new_node_covariate_int('z')
-net.node_covariates['z'][:] = net.node_covariates['value'][:]
-#net.node_covariates['z'][:] = np.random.random(N) < 0.5
+if from_truth:
+    net.node_covariates['z'][:] = net.node_covariates['value'][:]
+else:
+    net.node_covariates['z'][:] = np.random.random(N) < 0.5
 
 # Calculate NLL at truth
 fit_model.fit_sem(net, cycles = 1, sweeps = 0,
@@ -59,36 +67,43 @@ fit_model.fit_sem(net, cycles = 1, sweeps = 0,
 baseline_nll = fit_model.sem_trace[0][0]
 
 nll_trace = []
-z_trace = np.empty((reps,N))
+z_trace = np.empty((steps,N))
 disagreement_trace = []
+theta_trace = []
 
-for rep in range(reps):
-    print rep
+for step in range(steps):
+    print step
     fit_model.fit_sem(net, 1, 2, store_all = True)
     #fit_model.fit_kl(net, 1)
     nll_trace.append(fit_model.nll(net))
-    z_trace[rep,:] = net.node_covariates['z'][:]
+    z_trace[step,:] = net.node_covariates['z'][:]
     disagreement = minimum_disagreement(net.node_covariates['value'][:],
                                         net.node_covariates['z'][:])
     disagreement_trace.append(disagreement)
+    theta_trace.append(fit_model.base_model.beta['x'])
 
 # Eliminate symmetry of 'z'
-for rep in range(reps):
-    if np.mean(z_trace[rep,:]) < 0.5:
-        z_trace[rep,:] = 1 - z_trace[rep,:]
-z_trace += np.random.normal(0, 0.01, (reps, N))
+for step in range(steps):
+    if np.mean(z_trace[step,:]) < 0.5:
+        z_trace[step,:] = 1 - z_trace[step,:]
+z_trace += np.random.normal(0, 0.01, (steps, N))
                     
 nll_trace = np.array(nll_trace)
 nll_trace -= baseline_nll
 disagreement_trace = np.array(disagreement_trace)
-    
+
 plt.figure()
-plt.plot(np.arange(reps), nll_trace)
+plt.plot(np.arange(steps), theta_trace)
+plt.xlabel('step')
+plt.ylabel('theta')
+
+plt.figure()
+plt.plot(np.arange(steps), nll_trace)
 plt.xlabel('step')
 plt.ylabel('NLL')
 
 plt.figure()
-plt.plot(np.arange(reps), disagreement_trace)
+plt.plot(np.arange(steps), disagreement_trace)
 plt.xlabel('step')
 plt.ylabel('normalized disagreement')
 
@@ -102,7 +117,7 @@ try:
     pca = PCA(z_trace)
 
     plt.figure()
-    plt.plot(np.arange(reps), pca.Y[:,0])
+    plt.plot(np.arange(steps), pca.Y[:,0])
     plt.xlabel('step')
     plt.ylabel('z (PC1)')
 
@@ -131,6 +146,6 @@ try:
     plt.xlabel('z (PC1)')
     plt.ylabel('z (PC2)')
 except:
-    print 'PCA failed; maybe no variation in z?'
+    print 'PCA failed; maybe no variation in z or steps < N?'
 
 plt.show()    
