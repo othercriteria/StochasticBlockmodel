3ce495d3d08173083501ddd5a225bb76ddf33857
Daniel Klein <othercriteria@gmail.com>
2016-04-07 14:18:25 -0400

    Bring test_block.py up to date with rest of code

--- test_block.py
+++ test_block.py
@@ -8,12 +8,11 @@ import numpy as np
 from Network import Network
 from Models import StationaryLogistic, NonstationaryLogistic, Blockmodel
 from Models import alpha_zero, alpha_norm, alpha_unif, alpha_gamma
-from Experiment import RandomSubnetworks, Results, add_network_stats
+from Experiment import RandomSubnetworks, Results, add_array_stats
 from Experiment import minimum_disagreement, rel_mse
-from Utility import logit
 
 # Parameters
-params = { 'N': 600,
+params = { 'N': 100,
            'K': 2,
            'class_conc': 10.0,
            'Theta_diag': 4.0,
@@ -24,16 +23,16 @@ params = { 'N': 600,
            'alpha_unif': 0.0,
            'alpha_norm_sd': 2.0,
            'alpha_gamma_sd': 0.0,
-           'kappa_target': ('edges', 20),
+           'kappa_target': ('row_sum', 5),
            'fit_nonstationary': True,
            'fit_conditional': True,
            'fit_K': 2,
            'initialize_true_z': False,
-           'cycles': 20,
-           'sweeps': 2,
+           'cycles': 5,
+           'sweeps': 1,
            'verbose': False,
            'num_reps': 5,
-           'sub_sizes': range(25, 155, 25),
+           'sub_sizes': np.arange(10, 60, 5),
            'plot_mse': True,
            'plot_network': True }
 
@@ -62,17 +61,14 @@ else:
 
 # Generate covariates and associated coefficients
 data_base_model = NonstationaryLogistic()
-covariates = []
 for b in range(params['B']):
     name = 'x_%d' % b
-    covariates.append(name)
 
     data_base_model.beta[name] = np.random.normal(0, params['beta_sd'])
 
     def f_x(i_1, i_2):
         return np.random.uniform(-np.sqrt(3), np.sqrt(3))
     net.new_edge_covariate(name).from_binary_function_ind(f_x)
-
     
 # Initialize data (block)model from base model
 class_probs = np.random.dirichlet(np.repeat(params['class_conc'], params['K']))
@@ -91,28 +87,29 @@ if params['plot_network']:
 
 # Initialize fitting model
 fit_base_model = StationaryLogistic()
-for c in covariates:
-    fit_base_model.beta[c] = None
+for b in data_base_model.beta:
+    fit_base_model.beta[b] = None
 fit_model = Blockmodel(fit_base_model, params['fit_K'])
 if params['fit_nonstationary']:
     n_fit_base_model = NonstationaryLogistic()
-    for c in covariates:
-        n_fit_base_model.beta[c] = None
+    for b in data_base_model.beta:
+        n_fit_base_model.beta[b] = None
     n_fit_model = Blockmodel(n_fit_base_model, params['fit_K'])
 net.new_node_covariate_int('z')
 
 # Set up recording of results from experiment
-s_results = Results(params['sub_sizes'], params['num_reps'], 'Stationary fit')
-add_network_stats(s_results)
+s_results = Results(params['sub_sizes'], params['sub_sizes'],
+                    params['num_reps'], 'Stationary fit')
+add_array_stats(s_results)
 s_results.new('Subnetwork kappa', 'm', lambda d, f: d.base_model.kappa)
-def f_c(c):
-    return ((lambda d, f: d.base_model.beta[c]),
-            (lambda d, f: f.base_model.beta[c]))
-for c in covariates:
+def f_b(b):
+    return ((lambda d, f: d.base_model.beta[b]),
+            (lambda d, f: f.base_model.beta[b]))
+for b in data_base_model.beta:
     # Need to do this hackily to avoid for-loop/lambda-binding weirdness.
-    f_true, f_estimated = f_c(c)
-    s_results.new('True beta_{%s}' % c, 'm', f_true)
-    s_results.new('Estimated beta_{%s}' % c, 'm', f_estimated)
+    f_true, f_estimated = f_b(b)
+    s_results.new('True beta_{%s}' % b, 'm', f_true)
+    s_results.new('Estimated beta_{%s}' % b, 'm', f_estimated)
 s_results.new('Class mismatch', 'n',
               lambda n: minimum_disagreement(n.node_covariates['z_true'][:], \
                                              n.node_covariates['z'][:]))
@@ -121,9 +118,9 @@ def rel_mse_p_ij(n, d, f):
     return rel_mse(f.edge_probabilities(n), f.baseline(n), P)
 s_results.new('Rel. MSE(P)', 'nm', rel_mse_p_ij)
 def rel_mse_logit_p_ij(n, d, f):
-    logit_P = logit(d.edge_probabilities(n))
+    logit_P = d.edge_probabilities(n, logit = True)
     logit_Q = f.baseline_logit(n)
-    return rel_mse(logit(f.edge_probabilities(n)), logit_Q, logit_P)
+    return rel_mse(f.edge_probabilities(n, logit = True), logit_Q, logit_P)
 s_results.new('Rel. MSE(logit_P)', 'nm', rel_mse_logit_p_ij)
 
 all_results = { 's': s_results }
@@ -141,27 +138,28 @@ def initialize(s, f):
         s.node_covariates['z'][:] = s.node_covariates['z_true'][:]
     else:
         s.node_covariates['z'][:] = np.random.randint(0, params['fit_K'], s.N)
-        
+
 for sub_size in params['sub_sizes']:
-    print 'subnetwork size = %d' % sub_size
+    size = (sub_size, sub_size)
+    print 'subnetwork size = %s' % str(size)
     
-    gen = RandomSubnetworks(net, sub_size)
+    gen = RandomSubnetworks(net, size)
     for rep in range(params['num_reps']):
-        subnet = gen.sample()
+        subnet = gen.sample(as_network = True)
         data_model.match_kappa(subnet, params['kappa_target'])
         subnet.generate(data_model)
         
         initialize(subnet, fit_model)
         fit_model.fit(subnet, params['cycles'], params['sweeps'],
                       verbose = params['verbose'])
-        s_results.record(sub_size, rep, subnet, data_model, fit_model)
+        s_results.record(size, rep, subnet, data_model, fit_model)
         print
 
         if params['fit_conditional']:
             initialize(subnet, fit_model)
             fit_base_model.fit = fit_base_model.fit_conditional
             fit_model.fit(subnet, params['cycles'], params['sweeps'])
-            c_results.record(sub_size, rep, subnet, data_model, fit_model)
+            c_results.record(size, rep, subnet, data_model, fit_model)
             print
         
         if params['fit_nonstationary']:
@@ -169,18 +167,18 @@ for sub_size in params['sub_sizes']:
             initialize(subnet, n_fit_model)
             n_fit_model.fit(subnet, params['cycles'], params['sweeps'],
                             verbose = params['verbose'])
-            n_results.record(sub_size, rep, subnet, data_model, n_fit_model)
+            n_results.record(size, rep, subnet, data_model, n_fit_model)
             print
 
 # Compute beta MSEs
 covariate_mses = []
-for c in covariates:
-    name = 'MSE(beta_{%s})' % c
+for b in fit_base_model.beta:
+    name = 'MSE(beta_{%s})' % b
     covariate_mses.append(name)
     for model in all_results:
         results = all_results[model]
         results.estimate_mse(name,
-                             'True beta_{%s}' % c, 'Estimated beta_{%s}' % c)
+                             'True beta_{%s}' % b, 'Estimated beta_{%s}' % b)
 
 for model in all_results:
     results = all_results[model]
@@ -205,11 +203,10 @@ if params['plot_mse']:
 if params['plot_network']:
     s_results.title = None
     
-    s_results.plot([('Average out-degree', {'ymin': 0, 'plot_mean': True}),
-                    ('Average in-degree', {'ymin': 0, 'plot_mean': True}),
-                    (['Out-degree', 'Max out-degree', 'Min out-degree'],
+    s_results.plot([('Average row-sum', {'ymin': 0, 'plot_mean': True}),
+                    ('Average col-sum', {'ymin': 0, 'plot_mean': True}),
+                    (['row-sum', 'Max row-sum', 'Min row-sum'],
                      {'ymin': 0, 'plot_mean': True}),
-                    (['In-degree', 'Max out-degree', 'Min in-degree'],
+                    (['col-sum', 'Max row-sum', 'Min col-sum'],
                      {'ymin': 0, 'plot_mean': True}),
-                    ('Self-loop density', {'ymin': 0, 'plot_mean': True}),
                     'Subnetwork kappa'])
